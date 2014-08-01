9674c2ad537a9904649e93caf24d2ab1ce086889
char-lie <webmaster976@gmail.com>
2014-08-01 17:55:59 +0300

    Added Thesis directory

--- pull.sh
+++ pull.sh
@@ -7,6 +7,7 @@ git pull https://github.com/char-lie/diploma
 git add common/*.tex common/*.sty common/*.inc common/*.bib
 git add README.md
 git add practice/*.tex practice/index.pdf practice/*.latexmain practice/images/*.png
+git add thesis/*.tex thesis/index.pdf thesis/*.latexmain thesis/images/*.png
 git add pull.sh
 git commit -m "$1"
 git push https://char-lie@github.com/char-lie/diploma
