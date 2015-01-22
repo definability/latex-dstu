98405bc7f42ce0ee47d0d14b92b2fdaffeede3b0
char-lie <5962274+char-lie@users.noreply.github.com>
2015-01-22 22:17:57 +0200

    Changed code

--- thesis/code/plots.r
+++ thesis/code/plots.r
@@ -6,17 +6,19 @@ png('mygraph.png', width=18, height=18, units="cm", res=100);
 par(mfrow=c(n,m));
 sample.size = 6;
 lambda.weak = 1:6;
-lambda.avrg = rep(2,sample.size);
+lambda.avrg = rep(30,sample.size+1);
 for (i in 1:(n*m)) {
     start_rhythm <- sample(35:45, 1)
-    aux <- simNHP.fun(lambda=lambda.weak);
+    aux <- simNHP.fun(lambda=lambda.avrg);
     noise.max = 2;
     noise.min = 0;
     noise.mean = (noise.max + noise.min)/2
     noise <- rbinom(sample.size,noise.max,0.5) - noise.mean;
-    cur <- unlist((Map(function(x) start_rhythm - length(aux$posNH[aux$posNH==x]), 0:(sample.size - 1)))) + noise;
+    #cur <- unlist((Map(function(x) start_rhythm - length(aux$posNH[aux$posNH==x]), 0:(sample.size - 1)))) + noise;
+    cur <- unlist((Map(function(x) length(aux$posNH[aux$posNH==x]), 1:sample.size))) + noise;
     #plot(c(cur), xlab="Time", ylab="Taps", ylim=c(start_rhythm-10, start_rhythm+10));
-    plot(c(cur), xlab="Time", ylab="Taps");
+    cur = cur/cur[1] - 1;
+    plot(c(cur), xlab="Time", ylab="Taps", ylim=c(-.5,.5));
     #cur = cur/start_rhythm - 1;
     #plot(c(cur), xlab="Time", ylab="Taps", ylim=c(-0.2, 0.2));
     lines(c(cur));
