51dccd2f2e3bdfb52869b3051ab2697ddceb81b3
char-lie <webmaster976@gmail.com>
2015-04-01 22:54:33 +0300

    Added appendix

--- thesis/code/plots.r
+++ thesis/code/plots.r
@@ -1,39 +1,34 @@
 library(NHPoisson)
-
 wideScreen();
-
+# Read command line arguments
+args <- commandArgs(TRUE);
+displayApproximation <- FALSE;
+if (length(args) > 0 && args[1] == "approximation") {
+    displayApproximation <- TRUE;
+}
+# Init cells for charts
 n <- 3;
 m <- 3;
-png('mygraph.png', width=18, height=22, units="cm", res=300);
-#png('mygraph.png');
+png('output.png', width=18, height=22, units="cm", res=300);
 par(mfrow=c(n,m));
+# Parameters
 sample.size = 6;
-lambda.weak = 1:6;
-lambda.avrg = rep(30,sample.size+1);
-#lambda.default = c(43, 40, 38, 37, 38, 35)
-#lambda.default = c(40, 40, 40, 40, 40, 40);
-lambda.default = c(40, 38, 36, 34, 32, 30);
+time <- seq(5, 30, 5);
+lambda.default = c(40, 38, 36, 34, 32, 30, 0);
+# Fill every cell
 for (i in 1:(n*m)) {
-    start_rhythm <- sample(35:45, 1)
-    aux <- simNHP.fun(lambda=lambda.avrg);
-    noise.max = 2;
-    noise.min = 0;
-    noise.mean = (noise.max + noise.min)/2
-    #noise <- rbinom(sample.size,noise.max,0.5) - noise.mean;
-    #cur <- unlist((Map(function(x) start_rhythm - length(aux$posNH[aux$posNH==x]), 0:(sample.size - 1)))) + noise;
-    #cur <- unlist((Map(function(x) length(aux$posNH[aux$posNH==x]), 1:sample.size))) + noise;
-    cur <- unlist((Map(function(x) length(aux$posNH[aux$posNH==x]), 1:sample.size)));
-    #plot(c(cur), xlab="Time", ylab="Taps", ylim=c(start_rhythm-10, start_rhythm+10));
-    #cur = cur/cur[1] - 1;
-    #plot(c(cur), xlab="Time", ylab="Taps", ylim=c(-.5,.5));
-    plot(c(cur), xlab="Час", ylab="Кількість постукувань", ylim=c(20,40));
-    #abline(lm(c(cur) ~ poly(c(1,2,3,4,5,6), 2, raw=TRUE)));
-    time  <- c(1,2,3,4,5,6);
+    # Generate non-homogeneous Poisson process
+    aux <- simNHP.fun(lambda=lambda.default);
+    # Calculate number of events in every time interval (we have sample.size of them)
+    cur <- unlist((Map(function(x) length(aux$posNH[aux$posNH==x]), 1:(sample.size))));
+    # Calculate approximation
     model <- lm(c(cur) ~ poly(time, 2, raw=TRUE));
     predictedcounts <- predict(model);
-    lines(time, predictedcounts, col='red');
-    #cur = cur/start_rhythm - 1;
-    #plot(c(cur), xlab="Time", ylab="Taps", ylim=c(-0.2, 0.2));
-    lines(c(cur));
+    # Plot
+    plot(time, c(cur), xlab="Час", ylab="Кількість постукувань", ylim=c(20,50));
+    lines(time, c(cur));
+    if (displayApproximation) {
+        lines(time, predictedcounts, col='red');
+    }
 }
 dev.off();
