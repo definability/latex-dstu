90cfb18c11e6198fcdbfee00a40cc44f70109d18
char-lie <webmaster976@gmail.com>
2015-04-01 23:57:13 +0300

    Added conclusion to course work

--- thesis/code/plots.r
+++ thesis/code/plots.r
@@ -12,8 +12,8 @@ m <- 3;
 png('output.png', width=18, height=22, units="cm", res=300);
 par(mfrow=c(n,m));
 # Parameters
-sample.size = 6;
 time <- seq(5, 30, 5);
+sample.size = length(time);
 lambda.default = c(40, 38, 36, 34, 32, 30, 0);
 # Fill every cell
 for (i in 1:(n*m)) {
@@ -22,11 +22,11 @@ for (i in 1:(n*m)) {
     # Calculate number of events in every time interval (we have sample.size of them)
     cur <- unlist((Map(function(x) length(aux$posNH[aux$posNH==x]), 1:(sample.size))));
     # Calculate approximation
-    model <- lm(c(cur) ~ poly(time, 2, raw=TRUE));
+    model <- lm(cur ~ poly(time, 2, raw=TRUE));
     predictedcounts <- predict(model);
     # Plot
-    plot(time, c(cur), xlab="Час", ylab="Кількість постукувань", ylim=c(20,50));
-    lines(time, c(cur));
+    plot(time, cur, xlab="Час", ylab="Кількість постукувань", ylim=c(20,50));
+    lines(time, cur);
     if (displayApproximation) {
         lines(time, predictedcounts, col='red');
     }
