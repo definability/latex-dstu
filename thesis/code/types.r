b343ec17b028de686b86434f5a43d61004a77ba7
char-lie <webmaster976@gmail.com>
2015-04-29 11:29:00 +0300

    Updated thesis

--- thesis/code/types.r
+++ thesis/code/types.r
@@ -10,12 +10,15 @@ sample.size = length(sample.time)
 sample.prediction_degree = 2
 sample.poly = poly(sample.time, sample.prediction_degree, raw=TRUE)
 lambda.default = c(40, 38, 36, 34, 32, 30, 0)
+groups.number = 4
+groups.names = c("Помилки", "Прямі", "Спадні", "Проміжні", "Опуклі", "Ввігнуті")
 #lambda.default = rep(40, sample.size+1)
 
-png('output.png', width=18, height=22, units="cm", res=300)
+png('output.png', width=22, height=22, units="cm", res=300)
 par(mfrow=c(rows,columns))
 
 result = rep(-1, n)
+res <- rep(0, groups.number+2)
 for (j in 2:(rows*columns+1)) {
     set.seed(j)
     for (i in 1:n) {
@@ -30,14 +33,15 @@ for (j in 2:(rows*columns+1)) {
                               extrema[['min']], extrema[['max']])
     }
     elements_in_group <- function(x) length(result[result==x])
-    groups <- unlist(Map(elements_in_group, -1:3))
+    groups <- unlist(Map(elements_in_group, -1:groups.number))
 
-    #print(paste("Undefined", round(groups[1]/n, 2)))
-    #print(paste("Flat",      round(groups[2]/(n - groups[1]), 2)))
-    #print(paste("Weak",      round(groups[3]/(n - groups[1]), 2)))
-    #print(paste("Middle",    round(groups[4]/(n - groups[1]), 2)))
-    #print(paste("Movable",   round(groups[5]/(n - groups[1]), 2)))
     print(groups)
-    barplot(groups, ylim=c(0, 70),
-            names.arg=c("Помилки", "Прямі", "Спадні", "Проміжні", "Опуклі"))
+    res[[1]] = res[[1]] + groups[[1]]
+    res[[2]] = res[[2]] + groups[[2]]
+    res[[3]] = res[[3]] + groups[[3]]
+    res[[4]] = res[[4]] + groups[[4]]
+    res[[5]] = res[[5]] + groups[[5]]
+    res[[6]] = res[[6]] + groups[[6]]
+    barplot(groups, ylim=c(0, 70), names.arg=groups.names)
 }
+print(res/600)
