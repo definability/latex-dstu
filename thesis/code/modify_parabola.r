d634b8311f29b59e5f4f0640b6932b3f033a6edd
char-lie <webmaster976@gmail.com>
2015-06-09 00:39:01 +0300

    Improved examination

--- thesis/code/modify_parabola.r
+++ thesis/code/modify_parabola.r
@@ -1,8 +1,8 @@
-t.start  <- 0
-t.finish <- 30
+t.stretch.start  <- 0
+t.stretch.finish <- 30
 
 parabola.stretch <- function (a, b, c, t.finish.new) {
-    t.k <- t.finish/t.finish.new
+    t.k <- t.stretch.finish/t.finish.new
     return(function (t) {
         x <- t.k * t
         return(a*x^2+b*x+c)
