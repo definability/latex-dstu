4be602110be437569c57022b7b7aec9656a96ec0
char-lie <webmaster976@gmail.com>
2015-06-11 00:52:35 +0300

    Added classification

--- thesis/code/modify_parabola.r
+++ thesis/code/modify_parabola.r
@@ -3,8 +3,8 @@ t.stretch.finish <- 30
 
 parabola.stretch <- function (a, b, c, t.finish.new) {
     t.k <- t.stretch.finish/t.finish.new
-    return(function (t) {
+    function (t) {
         x <- t.k * t
-        return(a*x^2+b*x+c)
-    })
+        a*x^2+b*x+c
+    }
 }
